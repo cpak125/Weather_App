fd50aa2781e21212eb69f3c33f320fe48cc9ae53
Chris Pak <chris.pak125@gmail.com>
2019-04-19 13:38:13 -0400

    Create titles,form,and weather components

---
+++ src/components/Titles.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Titles extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Weather App</h1>
+                <p> Helps you find weather conditions in cities...</p>
+            </div>
+        )
+    }
+}

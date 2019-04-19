3fd23ad69b063826eb1bd124e8171da24d0d532f
Chris Pak <chris.pak125@gmail.com>
2019-04-19 14:26:12 -0400

    Created getWeather funtion

--- src/App.js
+++ src/App.js
@@ -4,12 +4,26 @@ import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
+const API_KEY = `${process.env.REACT_APP_WEATHER_API_KEY}`
+
 class App extends Component {
+
+  getWeather = async (e) => {
+    e.preventDefault()
+
+    const city = e.target.elements.city.value
+    const country = e.target.elements.country.value
+
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`)
+    const response = await api_call.json()
+    console.log(response)
+  }
+
   render() {
     return (
       <div>
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather} />
         <Weather />
       </div>
     )

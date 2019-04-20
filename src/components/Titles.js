908f22ac52be85f850df7efab50fc423e636145b
Chris Pak <chris.pak125@gmail.com>
2019-04-19 20:47:03 -0400

    Changed title styling

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -4,8 +4,8 @@ export default class Titles extends Component {
     render() {
         return (
             <div>
-                <h1 className="title-container_title">Weather </h1>
-                <p className="title-container_subtitle"> Helps you find weather conditions in cities...</p>
+                <h1 className="title-container__title">Weather </h1>
+                <p className="title-container__subtitle"> Helps you find weather conditions in cities...</p>
             </div>
         )
     }
